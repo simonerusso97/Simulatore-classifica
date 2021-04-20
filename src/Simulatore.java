d497ad1b71d9404e0453ed490c01524c2a9cd0e6
Simone Russo <97.russo@gmail.com>
2021-04-20 12:34:29 +0200

    Creata classe Business

--- src/Simulatore.java
+++ src/Simulatore.java
@@ -38,11 +38,12 @@ public class Simulatore {
 
 
         //CREO IL CALENDARIO
-        calendario = ListMatches(squadraArrayList);
+        calendario = Business.getInstance().ListMatches(squadraArrayList);
 
         //STAMPO IL CALENDARIO
-        stampaCalendario(calendario);
+        Business.getInstance().stampaCalendario(calendario);
         calendario.getGiornataArrayList().remove(0);
+
         //CALCOLO TUTTE LE COMBINAZIONI POSSIBILI DEGLI ESITI DELLE PARTITE
         Combinatorio.CombinationRepetition(squadraArrayList.size()/2);
         nEsitiPossibili=chosenArray.size();
@@ -124,61 +125,7 @@ public class Simulatore {
 
 
 
-    static Calendario ListMatches(ArrayList<Squadra> squadraArrayList){
 
 
-        int numDays = (squadraArrayList.size() - 1); // Days needed to complete tournament
-        int halfSize = squadraArrayList.size() / 2;
-
-        ArrayList<Squadra> teams = new ArrayList<>();
-        Partita partita;
-        Giornata giornata;
-        Calendario calendario = new Calendario();
-
-        teams.addAll(squadraArrayList); // Add teams to List and remove the first team
-        teams.remove(0);
-
-        int teamsSize = teams.size();
-
-        for (int day = 0; day < numDays; day++)
-        {
-            giornata = new Giornata(day);
-            int teamIdx = day % teamsSize;
-
-            partita= new Partita(teams.get(teamIdx), squadraArrayList.get(0));
-            giornata.getPartitaArrayList().add(partita);
-
-            for (int idx = 1; idx < halfSize; idx++)
-            {
-                int firstTeam = (day + idx) % teamsSize;
-                int secondTeam = (day  + teamsSize - idx) % teamsSize;
-                partita = new Partita(teams.get(firstTeam), teams.get(secondTeam));
-                giornata.getPartitaArrayList().add(partita);
-            }
-            calendario.getGiornataArrayList().add(giornata);
-        }
-        return calendario;
-    }
-
-    static void stampaCalendario(Calendario calendario){
-        int k=0;
-        for (Giornata gior: calendario.getGiornataArrayList()) {
-            System.out.println("GIORNATA "+k);
-            for (Partita par : gior.getPartitaArrayList()){
-                System.out.println(par.getSquadra1().getNome() +" - "+ par.getSquadra2().getNome());
-            }
-            System.out.println("---------------");
-            k++;
-        }
-    }
-    static class SortByPunti implements Comparator<Squadra>
-    {
-        // Used for sorting in ascending order of
-        // roll number
-        public int compare(Squadra a, Squadra b)
-        {
-            return a.punti - b.punti;
-        }
-    }
 
 }
---
+++ src/SortByPunti.java
@@ -0,0 +1,11 @@
+import java.util.Comparator;
+
+class SortByPunti implements Comparator<Squadra>
+{
+    // Used for sorting in ascending order of
+    // roll number
+    public int compare(Squadra a, Squadra b)
+    {
+        return a.punti - b.punti;
+    }
+}
